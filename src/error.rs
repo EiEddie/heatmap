3dc3688aeb2d32662b95c5528474f959c7003d40
EiEddie <eieddie4u@outlook.com>
2023-12-14 16:24:44 +0800

    feat: 错误处理

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+	/// 给定的日期不合法
+	#[error("Given date is wrong")]
+	DateError,
+
+	#[error(transparent)]
+	DataBaseError(#[from] ::rusqlite::Error),
+}
+
+pub type Result<T> = ::std::result::Result<T, Error>;
