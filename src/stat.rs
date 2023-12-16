740127ca0876565e902cb815bb5ae899d822a806
EiEddie <eieddie4u@outlook.com>
2023-12-16 17:09:08 +0800

    fix: `DaysData` 底层存储日序号

--- src/stat.rs
+++ src/stat.rs
@@ -5,14 +5,19 @@ use chrono::{Datelike, NaiveDate};
 /// 每一天的数据
 #[derive(Debug)]
 struct DaysData {
-	data: BTreeMap<NaiveDate, u32>,
-	// TODO: 只包含月份和日期的结构代替 [`NaiveDate`]
-	// 因为它与 [`YearData`] 中的信息重复
+	/// 储存的是哪一年的数据
+	year: i32,
+
+	/// 储存的值
+	/// - `key`: 距一年第一天的日数, 从 0 开始
+	/// - `val`: 这一天的统计数据
+	data: BTreeMap<u32, u32>,
 }
 
 impl DaysData {
-	fn new() -> Self {
-		return Self { data: BTreeMap::new(), };
+	fn new(year: i32) -> Self {
+		return Self { year,
+		              data: BTreeMap::new() };
 	}
 
 	/// 添加一条记录, 并返回给定日期的统计总次数
@@ -20,13 +25,13 @@ impl DaysData {
 	/// 若日期存在, 则给该日期的统计 +1;
 	/// 若不存在, 创建此日期的记录且 +1.
 	fn add(&mut self, date: NaiveDate) -> u32 {
-		let cnt = self.data.entry(date).or_insert(0);
+		let cnt = self.data.entry(date.ordinal0()).or_insert(0);
 		*cnt += 1;
 		return *cnt;
 	}
 
 	fn get(&self, date: &NaiveDate) -> Option<u32> {
-		return self.data.get(date).cloned();
+		return self.data.get(&date.ordinal0()).cloned();
 	}
 }
 
@@ -48,7 +53,8 @@ impl YearData {
 	/// 若日期存在, 则给该日期的统计 +1;
 	/// 若不存在, 创建此日期的记录且 +1.
 	pub fn add(&mut self, date: NaiveDate) -> u32 {
-		let days_data = self.data.entry(date.year()).or_insert(DaysData::new());
+		let year = date.year();
+		let days_data = self.data.entry(year).or_insert(DaysData::new(year));
 		return days_data.add(date);
 	}
 }
