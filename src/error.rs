c90f23f3f164cf4937ef4443ac7faf71492aca1d
EiEddie <eieddie4u@outlook.com>
2023-12-19 14:41:30 +0800

    feat: 错误处理

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
+use std::io;
+
+use crossterm::style::Stylize;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -24,3 +27,13 @@ pub enum Error {
 }
 
 pub type Result<T> = ::std::result::Result<T, Error>;
+
+pub fn error_handler(err: &Error, out: &mut impl io::Write) {
+	if let Error::IoError(io_err) = err {
+		if io_err.kind() == io::ErrorKind::BrokenPipe {
+			::std::process::exit(0);
+		}
+	}
+
+	writeln!(out, "{}: {}", "[error]".red(), err).ok();
+}
